3cab074cc3eb753601277a2405e4b62d67feef52
GUILLAUME GERARD <ggerard@r-gds.fr>
2025-01-24 11:24:12 +0100

    rename resource route from 'auteurs_back' to 'acteurs' and add catch-all route for welcome view

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('auteurs_back', App\Http\Controllers\AuteursController::class);
-Route::resource('categories_competences', App\Http\Controllers\CategorieCompetenceController::class)->middleware('can:edit_parametres_categories_competences,App\Models\Role');
+Route::resource('acteurs', App\Http\Controllers\AuteursController::class);
+
+Route::get('/{any}', function () {
+    return view('welcome');
+})->where('any', '.*');
